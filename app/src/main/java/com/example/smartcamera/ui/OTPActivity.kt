21e4a694c11de9552601927ae81668dc0639185a
khan-mujeeb <mujeebkhan1831@gmail.com>
2023-03-20 23:21:17 +0530

    colours added, icon added, btn created, otp activity

---
+++ app/src/main/java/com/example/smartcamera/ui/OTPActivity.kt
@@ -0,0 +1,12 @@
+package com.example.smartcamera.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.smartcamera.R
+
+class OTPActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_otpactivity)
+    }
+}
